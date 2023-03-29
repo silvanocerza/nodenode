e6c005dbae350aad3c7068f01b7d53376c5da242
Silvano Cerza <silvanocerza@gmail.com>
2023-03-29 17:28:27 +0200

    Test

---
+++ test.js
@@ -0,0 +1,17 @@
+const github = require("@actions/github");
+const path = require("path");
+const markup = require("markup-js");
+const { promises: fs } = require("fs");
+
+async function doStuff() {
+  const payloadFilePath =
+    "/Users/silvanocerza/workspace/haystack/.github/config/failure-message-slack-payload.json";
+  let payload = await fs.readFile(path.resolve(payloadFilePath), "utf-8");
+
+  const context = { github: github.context };
+  const payloadString = payload.replace("$", "");
+  payload = markup.up(payloadString, context);
+  console.log(payload);
+}
+
+doStuff();
